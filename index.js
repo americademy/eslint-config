1b9ffef487f92e31e44d06e3a6c66e5624406a92
ELLIOTTCABLE <git@ell.io>
2019-01-18 23:08:22 -0600

    (new) Suggest useless template-literal strings be downgraded to static quotes

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ module.exports = {
   rules: {
     strict: 0,
     'no-undef': 2,
+    'no-tabs': 'error',
+
+    // This is configured specifically to play well with Prettier. The name is misleading, it *does*
+    // allow `template-literal ${strings}`; it just advises you to convert them back into simple
+    // single-quotes if there's no interpolation.
+    quotes: ['quotes', 'double', { avoidEscape: true, allowTemplateLiterals: false }],
 
     'prettier/prettier': 'warn',
   },
